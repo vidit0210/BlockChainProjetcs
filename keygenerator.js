31363f372a882f9da942dbab72f85753c18f4e55
Vidit Shah <vidit0210@gmail.com>
2018-12-04 02:57:38 +0530

    CompleteImplementation

--- keygenerator.js
+++ keygenerator.js
@@ -1,9 +1,16 @@
-//Elliptic helps to make address and sign transacation
-const EC= require('elliptic').ec;
-//Type of Eliptical Curve 
+const EC = require('elliptic').ec;
+
+// You can use any elliptic curve you want
 const ec = new EC('secp256k1');
+
+// Generate a new key pair and convert them to hex-strings
 const key = ec.genKeyPair();
-const privateKey  = key.getPrivate('hex');
 const publicKey = key.getPublic('hex');
-console.log('Private key is:',privateKey);
-console.log('Public Key is :',publicKey);
+const privateKey = key.getPrivate('hex');
+
+// Print the keys to the console
+console.log();
+console.log('Your public key (also your wallet address, freely shareable)\n', publicKey);
+
+console.log();
+console.log('Your private key (keep this secret! To sign transactions)\n', privateKey);
--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-const {BlockChain,Transaction} = require('./blockchain');
-const StarkCoin = new BlockChain();
-StarkCoin.createTransaction = new Transaction('a1','a2',100);
-StarkCoin.createTransaction = new Transaction('a2','a1',50);
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-console.log('Starting the Miner');
-StarkCoin.minePendingTransaction('Hulk');
-console.log("Balance of Hulk is :",StarkCoin.getBalanceofAddress('Hulk'));
-//Balance will show 0 because it is in Mining Method
+// Your private key goes here
+const myKey = ec.keyFromPrivate(' 55daf0df0d3dbe0018ffd32ac0bc2237010bbf4990dad306a8bf24652c7581ba');
 
-console.log('Starting the Miner Again');
-StarkCoin.minePendingTransaction('Hulk');
-console.log("Balance of Hulk is :",StarkCoin.getBalanceofAddress('Hulk'));
+// From that we can calculate your public key (which doubles as your wallet address)
+const myWalletAddress = myKey.getPublic('hex');
+
+// Create new instance of Blockchain class
+const savjeeCoin = new Blockchain();
+
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 100);
+tx1.signTransaction(myKey);
+savjeeCoin.addTransaction(tx1);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+tx2.signTransaction(myKey);
+savjeeCoin.addTransaction(tx2);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(`Balance of xavier is ${savjeeCoin.getBalanceOfAddress(myWalletAddress)}`);
+
+// Uncomment this line if you want to test tampering with the chain
+// savjeeCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log('Blockchain valid?', savjeeCoin.isChainValid() ? 'Yes' : 'No');
